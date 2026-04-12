f40c7312d0bff343141d874cb1f3d345d5e7915d
Dhruvin D <dhruvinddev@gmail.com>
2026-04-12 22:09:34 +0530

    checkpoint

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -79,7 +79,7 @@ const Index = () => {
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{
-                  duration: 0.16,
+                  duration: 0.224,
                   ease: [0.215, 0.61, 0.355, 1],
                 }}
               >
